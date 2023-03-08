0b8c0ed4198431737c64dbf7035cb936b4daa0de
Jake <jakerandall1766@gmail.com>
2023-03-08 06:27:05 +0000

    working on json reader to extract total value of stock

--- read_json.py
+++ read_json.py
@@ -18,12 +18,23 @@ class Json_reader:
         for id in self.book_dict:
             print(self.book_dict[id]['title'])
 
-    def price_total:
- for item in
+    def price_total(self):
+        total = 0
+        for item in self.book_dict:
+            price = self.book_dict[item]['price']
+            print(price)
+            print(total)
+            price = price.replace(',', '')
+            if price != 'failed':
+                total += float(price)
+            elif price == 'failed':
+                print('invalid target...')
 
-# json_reader = Json_reader('previous_outputs/1_762.json')
-# json_reader.extract_json()
-# json_reader.print_titles()
+
+json_reader = Json_reader('previous_outputs/all_books.json')
+json_reader.extract_json()
+json_reader.print_titles()
+json_reader.price_total()
 
 
 
